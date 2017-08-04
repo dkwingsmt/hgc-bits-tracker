4d28f16f79f9631e21035ef832a2eeb41ffcaa82
Tong Mu <dkwingsmt@gmail.com>
2017-08-04 01:16:57 -0700

    Styles. Axis title. Team name. Limit init team number.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,46 @@ import moment from 'moment';
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
 import Chart from 'chart.js';
 
+const INIT_SHOW_TEAMS = 10;
+
+const teams = {
+  'blsm': 'Team BlossoM',
+  'ce': 'CE',
+  'dig': 'Team Dignitas',
+  'eid': 'Even In Death',
+  'estar': 'eStar',
+  'exp': 'Team Expert',
+  'fnc': 'Fnatic',
+  'gfe': 'Gale Force Esports',
+  'kt': 'KT',
+  'ldy': 'HotsLady',
+  'lfive': 'L5',
+  'mty': 'Mighty',
+  'mvpb': 'MVP Black',
+  'mvpm': 'MVP Miracle',
+  'nav': 'Team Naventic',
+  'nt': 'No Tomorrow',
+  'pd': 'Playing Ducks',
+  'roll': 'Roll20 esports',
+  'rpg': 'RPG',
+  'rrr': 'RRR',
+  'rvn': 'Raven',
+  'soa': 'SOA',
+  'spt': 'SPT',
+  'sss': 'Superstars',
+  'tf': 'Team Freedom',
+  'tgg': 'Team Good Guys',
+  'tl': 'Team Liquid',
+  'tmp': 'Tempest',
+  'trk': 'Tricked eSports',
+  'ts': 'Tempo Storm',
+  'wkg': 'WKG',
+  'zlt': 'Zealots'
+};
+
+const colors = ['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572',
+  '#FF9655', '#FFF263', '#6AF9C4'];
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -21,8 +61,14 @@ class App extends Component {
     })
     .then((tableData) => {
       const { date,     // eslint-disable-line no-unused-vars
-        time, progression, ...lines } = tableData;
+        time, progression, ...linesObject } = tableData;
       this.setState({ lastUpdateTime: _.last(time) });
+
+      const lines = _.sortBy(_.toPairs(linesObject),
+        ([_label, data]) => -_.last(data));
+
+      this.teams = _.map(lines, ([label]) => label);
+
       const ctx = document.getElementById('myChart').getContext('2d');
       const chart = new Chart(ctx, { // eslint-disable-line no-unused-vars
         // The type of chart we want to create
@@ -34,12 +80,16 @@ class App extends Component {
             label: 'Overall',
             data: progression,
             yAxisID: 'y-axis-1',
-            pointBorderColor: 'rgb(0, 0, 255)',
+            pointBorderColor: '#09469c',
+            pointStyle: 'triangle',
             fill: false
-          }].concat(_.map(lines, (data, label) => ({
-            label,
+          }].concat(_.map(lines, ([label, data], i) => ({
+            hidden: i >= INIT_SHOW_TEAMS,
+            label: teams[label],
             yAxisID: 'y-axis-0',
             fill: false,
+            pointStyle: 'rectRot',
+            borderColor: colors[i % colors.length],
             data
           })))
         },
@@ -52,15 +102,41 @@ class App extends Component {
                 displayFormats: {
                   'hour': 'MMM D, h:00A'
                 }
+              },
+              ticks: {
+                minRotation: 30
               }
             }],
             yAxes: [{
               'position': 'left',
-              'id': 'y-axis-0'
+              'id': 'y-axis-0',
+              'scaleLabel': {
+                'display': true,
+                'labelString': 'Team bits'
+              }
             }, {
               'position': 'right',
-              'id': 'y-axis-1'
+              'id': 'y-axis-1',
+              'scaleLabel': {
+                'display': true,
+                'labelString': 'Overall bits'
+              }
             }]
+          },
+          elements: {
+            line: {
+              tension: 0 // disables bezier curves
+            }
+          },
+          tooltips: {
+            callbacks: {
+              beforeTitle(tooltipItem) {
+                return tooltipItem.label;
+              },
+              title(tooltipItem) {
+                return moment(tooltipItem.x).format('MMM D, h:mmA');
+              }
+            }
           }
         }
       });
@@ -77,12 +153,13 @@ class App extends Component {
               <a>HGC Bits Tracker</a>
             </Navbar.Brand>
           </Navbar.Header>
-          <Nav>
-            <NavItem>{lastUpdateTime ? `Last update time: ${moment(lastUpdateTime).format('MMM D, h:mmA')}` : ''}</NavItem>
+          <Navbar.Text>{lastUpdateTime ? `Last update time: ${moment(lastUpdateTime).format('MMM D, h:mmA')}` : ''}</Navbar.Text>
+          <Nav pullRight>
+            <NavItem href="https://github.com/dkwingsmt/hgc-bits-tracker" pullRight>Github</NavItem>
           </Nav>
         </Navbar>
         <div style={{ maxWidth: 800, margin: 'auto' }}>
-          <canvas id="myChart" />
+          <canvas id="myChart" width={800} height={600} />
         </div>
       </div>
     );
