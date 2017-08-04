f68facd980f2a013e02df588641a61a0e3227bac
Tong Mu <dkwingsmt@gmail.com>
2017-08-04 14:11:28 -0700

    Update data

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-node update-data/dynamoDBtoCSV.js -t blizzard-bits-progression | tee data.csv | node update-data/parse.js > public/data.json
+node update-data/dynamoDBtoCSV.js -t blizzard-bits-progression | tee data.csv | node update-data/parse.js | tee public/data.json > docs/data.json
